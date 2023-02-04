1a31f732d27ef30083e60387aa596f1452df6dca
Denkhotieu <denkhotieu@gmail.com>
2023-02-04 12:59:40 +0700

    Add Audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : FastSingleton<AudioManager>
+{
+    static bool initialized = false;
+    static Dictionary<AudioName, AudioClip> audioClips = new Dictionary<AudioName, AudioClip>();
+    static AudioSource audioSource;
+
+    public bool Initialized
+    {
+        get { return initialized; }
+    }
+
+    public void Initialze(AudioSource source)
+    {
+        initialized = true;
+        audioSource = source;
+        audioClips.Add(AudioName.ButtonClick, Resources.Load<AudioClip>("ButtonClick"));
+        audioClips.Add(AudioName.Win, Resources.Load<AudioClip>("Win"));
+        audioClips.Add(AudioName.Start, Resources.Load<AudioClip>("Start"));
+        audioClips.Add(AudioName.MusicThemeSong, Resources.Load<AudioClip>("MusicThemeSong"));
+        audioClips.Add(AudioName.Lose, Resources.Load<AudioClip>("Lose"));
+    }
+
+    public void Play(AudioName name)
+    {
+        var audioTmp = audioClips[name];
+        if (audioTmp)
+        {
+            audioSource.PlayOneShot(audioClips[name]);
+        }
+        else
+        {
+            Debug.Log("audio Load that bai: " + name);
+        }
+        
+        
+    }
+
+    public void StopPlayAll()
+    {
+        audioSource.Stop(); 
+    }
+
+}
---
+++ Assets/Scripts/FastSingleton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// FastSingleton MonoBehaviour ---> For fasted reference purpose
+/// Update:2020-04-21 by hungtx
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class FastSingleton<T> : MonoBehaviour where T : FastSingleton<T> {
+	/// <summary>
+	/// The public static reference to the instance
+	/// Call if you sure the reference is available
+	/// </summary>
+	private static T _instance;
+	private static bool _instantiated; //checking bool is faster than checking null
+	/// <summary>
+	/// This is safe reference
+	/// Call to get reference in Awake Function
+	/// </summary>
+	public static T instance {
+		get {
+			if (_instantiated) {
+				return _instance;
+			}
+			_instance = (T) FindObjectOfType(typeof(T));
+			if (!_instance) {
+				GameObject gameObject = new GameObject(typeof(T).ToString());
+				return gameObject.AddComponent<T>();
+			}
+			if (_instance) {
+				_instantiated = true;
+			}
+			return _instance;
+		}
+	}
+
+	protected virtual void Awake() {
+		// Make instance in Awake to make reference performance uniformly.
+		if (!_instance) {
+			_instance = (T) this;
+			_instantiated = true;
+		}
+		// If there is an instance already in the same scene, destroy this script.
+		else if (_instance != this) {
+			Debug.LogWarning("Singleton " + typeof(T) + " is already exists.");
+			Destroy(this);
+		}
+	}
+	protected virtual void OnDestroy() {
+		if (_instance == this) {
+			_instance = null;
+			_instantiated = false;
+		}
+	}
+}
